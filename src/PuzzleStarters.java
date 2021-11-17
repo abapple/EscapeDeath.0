d445bcab393fff660d23d7095664c9bf6d1fa6a3
Abby Bock <abock_02@arcadia.edu>
2021-11-17 18:33:17 -0500

    Start of Library

--- src/PuzzleStarters.java
+++ src/PuzzleStarters.java
@@ -1,11 +1,12 @@
 import java.awt.im.InputContext;
 import java.io.*;
-import java.util.*;
+import java.util.*; 
 
 public class PuzzleStarters {
     public static HashMap<String, HashMap<Integer, String>> hints = new HashMap<>();
+
     // <Room, <HintNum, HintText>>
-    //hint(puzzleName, hintNumber)
+    // hint(puzzleName, hintNumber)
     public static int giveHint(String Room, int hintCnt) {
         switch (hintCnt) {
         case 3: {
@@ -65,7 +66,7 @@ public class PuzzleStarters {
     }
 
     public static void main(String[] args) {
-        //fluffy introductions 
+        // fluffy introductions
         readInHints();
         System.out.println("Lets Begin!!");
         Scanner sc = new Scanner(System.in);
@@ -81,32 +82,84 @@ public class PuzzleStarters {
             number = 15;
         recRoom(number, hintCnt);
 
-        /*
-         * //recursion room implementing queue and while loop boolean trapped = true;
-         * Queue<Character> directions = new LinkedList(); char[] map = { 'L', 'R', 'L',
-         * 'L', 'R', 'R' }; System.out.println("Find your way out!!"); while (trapped) {
-         * // fun hallway statement
-         * System.out.println("Press L for left or R for right"); // input L or R for
-         * direction // later implementation in application, use a L button or a R
-         * button String input = sc.nextLine();
-         * 
-         * char dir = input.charAt(0); if (dir == 'l' || dir == 'L') {
-         * directions.offer('L');
-         * 
-         * } else if (dir == 'r' || dir == 'R') { directions.offer('R');
-         * 
-         * } else if (input.equalsIgnoreCase("hint")) { hintCnt = giveHint("Recursion",
-         * hintCnt); System.out.println("Are you dumb?"); } else {
-         * System.out.println("Not an option my friend"); } if (directions.size() ==
-         * map.length) { for (int i = 0; i < map.length; i++) { if (directions.peek() ==
-         * map[i]) { directions.poll(); if (directions.isEmpty()) { trapped = false; } }
-         * else { System.out.println("You took a wrong turn!! Go back!"); // start at
-         * beginning directions.clear(); trapped = true; break; // put back at beginning
-         * } } } } System.out.println("You win"); // end of recursion room
-         */
+       
+
+    }
+
+    public static void library_morse(){
+
+    }
+    public static void library_books(int hintCnt){
+        //THE KEY TO GET HERE IS MYSTERY 
+        Scanner sc = new Scanner(System.in);
+        boolean trapped = true;
+        // recursion room implementing queue and while loop boolean trapped = true;
+        Queue<Character> directions = new LinkedList();
+        char[] map = { 'L', 'R', 'L', 'L', 'R', 'R' };
+
+        HashMap<String, String> books = new HashMap();
+        books.put("Agatha Christie", "The Murder on the Orient Express");
+        books.put("", "The Murder on the Orient Express");
+        books.put("Agatha Christie", "The Murder on the Orient Express");
+        books.put("Agatha Christie", "The Murder on the Orient Express");
+        books.put("Agatha Christie", "The Murder on the Orient Express");
+        books.put("Agatha Christie", "The Murder on the Orient Express");
+
+        String code = "Escape";
+        //authors names author.charAt(0) add to String completeCOde
+        //compare that string to code above
+
+        String in = sc.nextLine();
+
+
+
+
+
+
+
+        System.out.println("Find your way out!!");
+        while (trapped) {
+            // fun hallway statement
+            System.out.println("Press L for left or R for right"); // input L or R for
+            // direction // later implementation in application, use a L button or a button
+            String input = sc.nextLine();
+
+            char dir = input.charAt(0);
+            if (dir == 'l' || dir == 'L') {
+                directions.offer('L');
+
+            } else if (dir == 'r' || dir == 'R') {
+                directions.offer('R');
+
+            } else if (input.equalsIgnoreCase("hint")) {
+                hintCnt = giveHint("Recursion", hintCnt);
+                System.out.println("Are you dumb?");
+            } else {
+                System.out.println("Not an option my friend");
+            }
+            if (directions.size() == map.length) {
+                for (int i = 0; i < map.length; i++) {
+                    if (directions.peek() == map[i]) {
+                        directions.poll();
+                        if (directions.isEmpty()) {
+                            trapped = false;
+                        }
+                    } else {
+                        System.out.println("You took a wrong turn!! Go back!"); // start a tbeginning
+                        directions.clear();
+                        trapped = true;
+                        break; // put back at beginning
+                    }
+                }
+            }
+        }
+        System.out.println("You win"); // end of recursion room
+        sc.close();
     }
 
     public static void recRoom(int turns, int hintCnt) {
+        // door will be turns num; doors are "locked" until
+        // finding door #10
         System.out.println(turns);
         if (turns == 10) {
             System.out.println("You made it!");
@@ -114,8 +167,7 @@ public class PuzzleStarters {
         } else {
             if (turns >= 7 && turns <= 13) {
                 tauntPlayer(true);
-            }
-            else if(turns <= 0 || turns >=20){
+            } else if (turns <= 0 || turns >= 20) {
                 tauntPlayer(false);
             }
             Scanner in = new Scanner(System.in);
@@ -138,28 +190,27 @@ public class PuzzleStarters {
         }
     }
 
-    public static void tauntPlayer(boolean close){
+    public static void tauntPlayer(boolean close) {
         ArrayList<String> enCourage = new ArrayList<>();
         ArrayList<String> disCourage = new ArrayList<>();
 
         Scanner tnt = null;
         try {
-           tnt = new Scanner(new File("src/taunts.txt"));
+            tnt = new Scanner(new File("src/taunts.txt"));
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        for(int i = 0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             enCourage.add(tnt.nextLine());
         }
-        for(int i = 0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             disCourage.add(tnt.nextLine());
         }
-        if(close){
+        if (close) {
             int number = 0 + (int) (Math.random() * ((4 - 0) + 1));
             System.out.println(enCourage.get(number));
-        }
-        else{
+        } else {
             int number = 0 + (int) (Math.random() * ((4 - 0) + 1));
             System.out.println(disCourage.get(number));
         }
