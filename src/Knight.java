1ad132949deadeb5720911ec19d82c068f8d6643
Abby Bock <abock_02@arcadia.edu>
2021-12-08 13:39:03 -0500

    addKnight

---
+++ src/Knight.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class Knight {
+    public int x;
+    public int y;
+    public char symbol;
+    Knight(int[] p){
+        x = p[0]-1;
+        y = p[1];
+        symbol = 'K';
+    }
+    public boolean move(char dir, char[][] grid){
+        int temX;
+        int temY;
+        switch(dir){
+            case 'u':{
+                temX = x+1;
+                if(grid[temX][y] == 'o'){
+                    System.out.println("That is a wall, cannot go there.");
+                    return false;
+                }else{
+                    x = temX;
+                }
+                break;
+            }
+            case 'd':{
+                temX = x-1;
+                if(grid[temX][y] == 'o'){
+                    System.out.println("That is a wall, cannot go there.");
+                    return false;
+                }else{
+                    x = temX;
+                }
+                break;
+            }
+            case 'l':{
+                temY = y+1;
+                if(grid[x][temY] == 'o'){
+                    System.out.println("That is a wall, cannot go there.");
+                    return false;
+                }else{
+                    y = temY;
+                }
+                break;
+            }
+            case 'r':{
+                temY = y+1;
+                if(grid[x][temY] == 'o'){
+                    System.out.println("That is a wall, cannot go there.");
+                    return false;
+                }else{
+                    y = temY;
+                }
+                break;
+            }
+            default:{
+                System.out.println("Invalid");
+                return false;
+            }
+        }
+        return false;
+    }
+}
--- src/map.java
+++ src/map.java
@@ -1,43 +1,60 @@
 import java.util.*;
+
 public class map {
-    final static int NUM_OF_STEPS = 10;
-	final static int GRID_SIZE = 6; 
+    final static int GRID_SIZE = 6;
     static char[][] grid;
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         grid = new char[GRID_SIZE][GRID_SIZE];
-		
-		for(int i = 0; i < GRID_SIZE ; i++){
-			for (int j = 0; j < GRID_SIZE ; j++){
-				grid[i][j] = 'o';
-			}
-		}
-        //method to input spaces(path) into grid array
-        HashMap<Integer, Queue<Integer>> mazes = new HashMap();
-        Queue<Integer> path = new LinkedList();
-        int[] p1 = {1, 4, 2, 4, 2, 3, 2, 2, 3, 2, 4, 2, 4, 3, 5, 3, 6, 3};
-        for(int i : p1){
-            path.offer(i);
+
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                grid[i][j] = 'o';
+            }
         }
-        mazes.put(1, path);
-        makeMaze(mazes);
-        //printing out the maze
-        System.out.println();
-		for(int i = 0; i < GRID_SIZE; i++){
-			for (int j = 0; j < GRID_SIZE ; j++)
-			{
-				System.out.print(grid[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println();
+        // method to input spaces(path) into grid array
+        HashMap<Integer, int[]> mazes = new HashMap();
+        int[] p1 = { 1, 4, 2, 4, 2, 3, 2, 2, 3, 2, 4, 2, 4, 3, 5, 3, 6, 3 };
+        mazes.put(1, p1);
+        // p1 = {};
+        // mazes.put(2, p1);
+        // p1 = {};
+        // mazes.put(3, p1);
+        // p1 = {};
+        // mazes.put(4, p1);
+
+        // randomize the maze
+        int mazeNum = 1 + (int) (Math.random() * (((mazes.size() - 1) - 1) + 1));
+        // Queue<Integer> path = new LinkedList();
+        // for (int i : mazes.get(mazeNum)) {
+        // path.offer(i);
+        // }
+        Knight user = new Knight(mazes.get(mazeNum));
+        makeMaze(mazes.get(mazeNum));
+        printMaze(user);
+        // printing out the maze
+
     }
-    public static void makeMaze(HashMap<Integer, Queue<Integer>> map){
-        //randomize the maze
-        int mazeNum = 1+ (int) (Math.random() * ((3 - 1) + 1));;
-        Queue<Integer> path = map.get(mazeNum);
-        while(!path.isEmpty()){
-                grid[path.poll()-1][path.poll()-1]= ' ';
+
+    public static void makeMaze(int[] path) {
+        // while (!path.isEmpty()) {
+        // grid[path.poll() - 1][path.poll() - 1] = ' ';
+        // }
+        for (int i = 1; i < path.length; i += 2) {
+            grid[path[i - 1] - 1][path[i]] = ' ';
         }
+
+    }
+
+    public static void printMaze(Knight user) {
+        grid[user.x][user.y] = user.symbol;
+        System.out.println();
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
         }
-        
+        System.out.println();
+    }
 }
